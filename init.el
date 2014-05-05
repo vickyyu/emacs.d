93cfccb2c30ad516d6beed4fdbf5f052d88b6c0c
Vicky Yu <vicky_yu@5vtechnologies.com>
2014-05-05 14:08:52 +0800

    Don't save session information
    Signed-off-by: Vicky Yu <vicky_yu@5vtechnologies.com>

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
 (require 'init-hippie-expand)
 (require 'init-auto-complete)
 (require 'init-windows)
-(require 'init-sessions)
+;(require 'init-sessions)
 (require 'init-fonts)
 (require 'init-mmm)
 
