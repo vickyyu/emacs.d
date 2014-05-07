b526f58600c0b94c0a9fe003237a728d6e76a64e
vicky_yu <vickyyu@5vtechnologies.com>
2014-05-07 16:56:04 +0800

    Modify syntax table for word syntax

--- lisp/init-syntax-table.el
+++ lisp/init-syntax-table.el
@@ -3,12 +3,12 @@
 ;;; Code:
 
 ;; Example : Add '_' in syntax table (the word definition will include '_').
-;;
-;; (dolist (hook '(makefile-gmake-mode-hook
-;;		makefile-bsdmake-mode-hook
-;;		c-mode-common-hook))
-;;   (add-hook hook
-;;	    (lambda () (modify-syntax-entry ?_ "w"))))
+
+(dolist (hook '(makefile-gmake-mode-hook
+		makefile-bsdmake-mode-hook
+		c-mode-common-hook))
+  (add-hook hook
+	    (lambda () (modify-syntax-entry ?_ "w"))))
 
 (provide 'init-syntax-table)
 ;;; init-syntax-table.el ends here
