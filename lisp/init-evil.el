6838f1f00600b82c254ba2700325065de0022c1a
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2014-05-20 11:10:09 +0800

    evil: Initialize cscope in normal state
    Conflicts:
    	lisp/init-evil.el

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -69,7 +69,6 @@
         (magit-commit-mode . normal)
 	(git-commit-mode . insert)
         (js2-error-buffer-mode . emacs)
-	(cscope-list-entry-mode . normal)
         (fundamental-mode . normal)
 	(help-mode . normal)
         )
@@ -80,7 +79,7 @@
 ;; when hooking to mode
 (loop for (hook) in
       '(
-	(cscope-list-entry-hook)
+;;	(cscope-list-entry-hook)
 	)
       do (add-hook hook 'evil-change-to-initial-state))
 
