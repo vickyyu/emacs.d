723e4e15d2f4bc5b9dcf2dd719db110dad727a03
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2014-10-01 18:12:31 +0800

    init-sr-speedbar: Fix init. error: void-function sr-speedbar-handle-other-window-advice
    * The multi-term has considered sr-speedbar package

--- init.el
+++ init.el
@@ -118,6 +118,7 @@
 ;; Additonal packages exclude with purcell init setting
 ;; Most settings are ported by https://github.com/redguardtoo/emacs.d
 (require 'init-color-theme)
+(require 'init-sr-speedbar)
 (require 'init-term-mode)
 (require 'init-cc-mode)
 (require 'init-xcscope)
@@ -132,7 +133,6 @@
 ;(require 'init-ctags) ;Enable if necessary
 (require 'init-buffer-move)
 (require 'init-web-mode)
-(require 'init-sr-speedbar)
 (require 'init-smartparens)
 ;; (require 'init-company)
 (require 'init-yasnippet)
