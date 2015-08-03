3e895da7aa6e2085f7e34868b682c4d23be67d8b
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2015-08-03 17:39:31 +0800

    python: Use ipython as a interpreter

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,3 +1,7 @@
+(require 'python)
+(setq python-shell-interpreter "ipython")
+;; (setq python-shell-interpreter-args "--pylab")
+
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
 		("SConscript\\'" . python-mode))
