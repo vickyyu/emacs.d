eb28a3bd601eca1b14dda08fddb4cecea6c4b0cf
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2015-08-04 16:49:46 +0800

    python:cscope: Hook cscoope-keymap when pycscope is found
    * Install pycscope by "pip install pycscope"

--- lisp/init-xcscope.el
+++ lisp/init-xcscope.el
@@ -10,4 +10,11 @@
 ;; emacs will call C-c s I update Index will mismatch cscope -q
 ;(setq cscope-option-do-not-update-database "t")
 
+;; Display the entry at point in one window.
+;; (define-key cscope-list-entry-keymap [return] 'cscope-select-entry-one-window)
+;; (define-key cscope-list-entry-keymap "\r" 'cscope-select-entry-one-window)
+
+(when (executable-find "pycscope")
+  (add-hook 'python-mode-hook (function cscope-minor-mode)))
+
 (provide 'init-xcscope)
