f2e6122ae9fe938ae4590531c49a21af14c5d131
Sherman Hsu <nonnn@localhost>
2015-10-07 09:46:14 +0800

    color-theme: Change data type from 'string' to 'color'

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -10,22 +10,22 @@
 ;; {{ Set background color
 (defun init-color-theme-set-bg-color (color)
   "Set background COLOR."
-  (when (not(eq color "nil"))
+  (when (not(eq color "black"))
     (set-face-background 'default color nil)
     (set-background-color color)))
 
-(defcustom init-color-theme-window-system-bg-color "nil"
+(defcustom init-color-theme-window-system-bg-color "black"
   "Set background color when \"window-system\". If nil uses default background."
-  :type '(string)
+  :type 'color
   :group 'init-color-theme
   :set #'(lambda (sym value)
 	   (when window-system
 	     (init-color-theme-set-bg-color value)))
   )
 
-(defcustom init-color-theme-no-window-system-bg-color "nil"
+(defcustom init-color-theme-no-window-system-bg-color "black"
   "Set background color when not a \"window-system\". If nil uses default background."
-  :type '(string)
+  :type 'color
   :group 'init-color-theme
   :set #'(lambda (sym value)
 	   (unless window-system
