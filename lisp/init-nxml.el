85b34cb005123964e0d8a6a414b9f3ccc890ba09
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2015-10-07 14:19:56 +0800

    init-nxml: XML Folding

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -1,3 +1,5 @@
+(require 'nxml-mode)
+
 (add-auto-mode
  'nxml-mode
  (concat "\\."
@@ -31,6 +33,20 @@ indentation rules."
       (backward-char) (insert "\n"))
     (nxml-mode)
     (indent-region begin end)))
+
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
 
 ;;----------------------------------------------------------------------------
 ;; Integration with tidy for html + xml
