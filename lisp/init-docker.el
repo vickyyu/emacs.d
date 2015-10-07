4f0d2c40bf0a23e2ad116cc1c9cc5d6f3015d182
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2015-10-07 22:38:13 +0800

    docker: Add and enable docker.el

--- lisp/init-docker.el
+++ lisp/init-docker.el
@@ -4,9 +4,18 @@
 ;; {{@see
 ;; https://github.com/spotify/dockerfile-mode
 ;; }}
+
 (require-package 'dockerfile-mode)
 
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+
+
+;; @See https://github.com/Silex/docker.el
+(require-package 'docker)
+
+;; The default keymap prefix is 'C-c d'
+(docker-global-mode 1)
 
 (provide 'init-docker)
 ;;; init-docker.el ends here
