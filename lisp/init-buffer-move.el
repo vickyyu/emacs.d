e5a37c086031ace16d8b6e751d44e4db85275d8d
Sherman Hsu <nonnn@localhost>
2015-10-07 09:48:15 +0800

    buffer-move: Change prefix to 'C-x 4'
    * C-x 4 will have guide-key help instruction
    * More intuition

--- lisp/init-buffer-move.el
+++ lisp/init-buffer-move.el
@@ -3,9 +3,9 @@
 (require 'buffer-move)
 
 ;; buffer-move.el
-(global-set-key (kbd "C-c C-b <up>")     'buf-move-up)
-(global-set-key (kbd "C-c C-b <down>")   'buf-move-down)
-(global-set-key (kbd "C-c C-b <left>")   'buf-move-left)
-(global-set-key (kbd "C-c C-b <right>")  'buf-move-right)
+(global-set-key (kbd "C-x 4 <up>")     'buf-move-up)
+(global-set-key (kbd "C-x 4 <down>")   'buf-move-down)
+(global-set-key (kbd "C-x 4 <left>")   'buf-move-left)
+(global-set-key (kbd "C-x 4 <right>")  'buf-move-right)
 
 (provide 'init-buffer-move)
